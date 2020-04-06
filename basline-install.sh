afd7578ccfa7e6826f7c3e22e6d13e4ea6364764
Yih Chuang <Yih.Chuang@gmail.com>
2020-04-06 10:11:23 -0700

    add baseline-install.sh as cofirmation

---
+++ basline-install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "Installation Started"
+
+sudo apt-get update -y
+sudo apt install default-jre -y
+sudo apt install openjdk-11-jre-headless -y
+sudo apt install openjdk-8-jre-headless -y
+sudo apt install unzip -y
+
+# antivirus
+wget http://sophoslinux.microsoft.com/SophosUpdate/savinstpkg.tgz && sudo tar -zxvf savinstpkg.tgz -C /opt 
+sudo /opt/sophos-av/install.sh
+
+# create a temp directory
+mkdir test123
+
+echo "Installation Completed"
